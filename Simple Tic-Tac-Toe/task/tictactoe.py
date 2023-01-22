c97fecf3574c77fa828ba242cb4fec2e3d825272
Kirill Nevinskii <mail@kirillnev.ru>
2023-01-22 12:01:51 +0600

    Stage_5_5

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -3,16 +3,6 @@ SYMBOL_O = 'O'
 SYMBOL_EMPTY = '_'
 
 
-def is_input_correct(string):
-    if len(string) == 9:
-        for i in range(9):
-            if string[i] != SYMBOL_X and string[i] != SYMBOL_O and string[i] != SYMBOL_EMPTY:
-                return False
-        return True
-    else:
-        return False
-
-
 def matrix_to_string(matrix):
     string = "---------"
     for i in range(3):
@@ -30,30 +20,17 @@ def matrix_to_string(matrix):
     return string
 
 
-def string_to_matrix(string):
-    matrix = []
-    for i in range(3):
-        matrix.append([])
-        for j in range(3):
-            value = 1 if string[i * 3 + j] == SYMBOL_X else 2 if string[i * 3 + j] == SYMBOL_O else 0
-            matrix[i].append(value)
-    return matrix
-
-
-def is_matrix_right(matrix):
-    count_x, count_o, count_empty = 0, 0, 0
-    for i in range(3):
-        for j in range(3):
-            count_x += 1 if matrix[i][j] == 1 else 0
-            count_o += 1 if matrix[i][j] == 2 else 0
-            count_empty += 1 if matrix[i][j] == 0 else 0
-    return abs(count_x - count_o) <= 1 and count_x + count_o + count_empty == 9
-
-
+# Returns 1 or 2 if someone wins
+# 0 if there are free cells
+# -1 if it is a draw
 def check_col(matrix, col):
     result = 1
     for i in range(3):
         result *= matrix[i][col]
+    if 1 < result < 8:
+        result = -1
+    elif result == 8:
+        result = 2
     return result
 
 
@@ -61,6 +38,10 @@ def check_row(matrix, row):
     result = 1
     for i in range(3):
         result *= matrix[row][i]
+    if 1 < result < 8:
+        result = -1
+    elif result == 8:
+        result = 2
     return result
 
 
@@ -68,6 +49,10 @@ def check_diagonal(matrix, diagonal):
     result = 1
     for i in range(3):
         result *= matrix[i][i] if diagonal == 0 else matrix[i][2 - i]
+    if 1 < result < 8:
+        result = -1
+    elif result == 8:
+        result = 2
     return result
 
 
@@ -75,23 +60,16 @@ def check_result(matrix):
     rows_result = [check_row(matrix, i) for i in range(3)]
     cols_result = [check_col(matrix, i) for i in range(3)]
     diagonals_result = [check_diagonal(matrix, i) for i in range(2)]
-    result = 0
+    result_list = rows_result + cols_result + diagonals_result
 
-    if 1 in rows_result and 8 in rows_result or \
-        1 in cols_result and 8 in cols_result or \
-        1 in diagonals_result and 8 in diagonals_result:
-        raise ValueError
-
-    if 1 in rows_result or 1 in cols_result or 1 in diagonals_result:
-        result = 1
-    elif 8 in rows_result or 8 in cols_result or 8 in diagonals_result:
-        result = 2
-    elif 0 in rows_result or 0 in cols_result or 0 in diagonals_result:
-        result = 0
+    if 1 in result_list:
+        return 1
+    elif 2 in result_list:
+        return 2
+    elif not all(result_list):
+        return 0
     else:
-        result = -1
-
-    return result
+        return -1
 
 
 def print_result(result):
@@ -99,8 +77,6 @@ def print_result(result):
         print("X wins")
     elif result == 2:
         print("O wins")
-    elif result == 0:
-        print("Game is not finished")
     else:
         print("Draw")
 
@@ -109,7 +85,7 @@ def is_cell_empty(matrix, i, j):
     return matrix[i][j] == 0
 
 
-def input_cell(matrix):
+def input_cell(matrix, cell_value):
     is_exit = False
     while not is_exit:
         try:
@@ -119,7 +95,7 @@ def input_cell(matrix):
             if 1 <= i <= 3 and 1 <= j <= 3:
                 if is_cell_empty(matrix, i - 1, j - 1):
                     is_exit = True
-                    matrix[i - 1][j - 1] = 1
+                    matrix[i - 1][j - 1] = cell_value
                 else:
                     print("This cell is occupied! Choose another one!")
             else:
@@ -129,20 +105,20 @@ def input_cell(matrix):
 
 
 def go():
-    input_string = input()
-    try:
-        if not is_input_correct(input_string):
-            raise ValueError
-        matrix = string_to_matrix(input_string)
-        print(matrix_to_string(matrix))
-        if not is_matrix_right(matrix):
-            raise ValueError
+    matrix = [[0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0]]
+    print(matrix_to_string(matrix))
+    result = 0
+    cell_value = 1
 
-        input_cell(matrix)
+    while result == 0:
+        input_cell(matrix, cell_value)
+        cell_value = 2 if cell_value == 1 else 1
         print(matrix_to_string(matrix))
+        result = check_result(matrix)
 
-    except ValueError:
-        print("Impossible")
+    print_result(result)
 
 
 if __name__ == "__main__":
